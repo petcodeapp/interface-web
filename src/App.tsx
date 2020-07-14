151626da8680212bd72ada46f6312f0b5f51084b
Rishi Kothari <itsrishikothari@gmail.com>
2020-07-14 14:57:56 -0400

    update app deps

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,44 @@
-import React from 'react';
-import {useObserver} from 'mobx-react-lite';
-import { AuthProvider, AuthContext } from './views/AuthContext';
-import firebase from 'firebase';
-import { auth } from './firebase/index';
+import React from "react";
+import { useObserver } from "mobx-react-lite";
+import { AuthProvider, AuthContext } from "./views/Auth";
+import firebase from "firebase";
+import { auth } from "./firebase/index";
 
 const Xe = () => {
   const store = React.useContext(AuthContext);
 
   return useObserver(() => (
-  <div>
-    {
-      !!store.user ? (
-      <h1>{JSON.stringify(store.user)}</h1>
-      ) : (<>
-        <h1>hi</h1>
+    <div>
+      {!!store.user ? (
+        <>
+        <h1>{JSON.stringify(store.user)}</h1>
         <button onClick={() => {
-          const goo = new firebase.auth.GoogleAuthProvider()
-          auth.signInWithPopup(goo);
-        }}></button>
-        </>
-      )
-    }
+          store.signOut();
+        }}>
 
-  </div>))
-}
+        </button>
+        </>
+      ) : (
+        <>
+          <h1>hi</h1>
+          <button
+            onClick={() => {
+              const goo = new firebase.auth.GoogleAuthProvider();
+              auth.signInWithPopup(goo);
+            }}
+          ></button>
+        </>
+      )}
+    </div>
+  ));
+};
 
 function App() {
   return (
     <>
-    <AuthProvider>
-      <Xe />
-    </AuthProvider>
+      <AuthProvider>
+        <Xe />
+      </AuthProvider>
     </>
   );
 }
