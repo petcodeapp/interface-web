2aaad8d75d25a8048ac8e4e484a25cc7ce4488d2
Rishi Kothari <itsrishikothari@gmail.com>
2020-07-14 14:42:33 -0400

    add authcontext

---
+++ src/views/AuthContext.tsx
@@ -0,0 +1,15 @@
+import React, { createContext } from "react";
+import Auth from '../modules/AuthModule';
+
+const AuthStore = new Auth();
+
+export const AuthContext = createContext(AuthStore);
+
+export const AuthProvider: React.FunctionComponent = ({ children }) => {
+    
+    return (
+        <AuthContext.Provider value={AuthStore}>
+            { children }
+        </AuthContext.Provider>
+    )
+}
