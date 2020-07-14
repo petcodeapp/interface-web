e0b63bcc21f0dd236ce4bf27f3e05cf729f8043e
Rishi Kothari <itsrishikothari@gmail.com>
2020-07-14 16:16:40 -0400

    update env firebase

--- src/firebase/index.ts
+++ src/firebase/index.ts
@@ -1,16 +1,7 @@
 import firebase from 'firebase';
 import environment from '../environment/environment';
 
-firebase.initializeApp({
-    apiKey: "AIzaSyAoHlfhxjCJsF45xPFfNHbG3bWQsuOHJXQ",
-    authDomain: "petcode-b1e6e.firebaseapp.com",
-    databaseURL: "https://petcode-b1e6e.firebaseio.com",
-    projectId: "petcode-b1e6e",
-    storageBucket: "petcode-b1e6e.appspot.com",
-    messagingSenderId: "858374127167",
-    appId: "1:858374127167:web:6b0270a202b7a666ad828a",
-    measurementId: "G-RHSZE35L55"
-});
+firebase.initializeApp(environment.firebaseConfig);
 
 export const auth = firebase.auth();
 
