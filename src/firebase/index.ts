51afb4755790886c054374e600794b6302099905
Rishi Kothari <itsrishikothari@gmail.com>
2020-07-14 16:15:05 -0400

    update gitignore

--- src/firebase/index.ts
+++ src/firebase/index.ts
@@ -1,6 +1,17 @@
 import firebase from 'firebase';
-import environment from '../../environment/environment';
+import environment from '../environment/environment';
 
-export const f = firebase.initializeApp(environment.firebaseConfig);
+firebase.initializeApp({
+    apiKey: "AIzaSyAoHlfhxjCJsF45xPFfNHbG3bWQsuOHJXQ",
+    authDomain: "petcode-b1e6e.firebaseapp.com",
+    databaseURL: "https://petcode-b1e6e.firebaseio.com",
+    projectId: "petcode-b1e6e",
+    storageBucket: "petcode-b1e6e.appspot.com",
+    messagingSenderId: "858374127167",
+    appId: "1:858374127167:web:6b0270a202b7a666ad828a",
+    measurementId: "G-RHSZE35L55"
+});
 
-export const { firestore, auth } = f;
+export const auth = firebase.auth();
+
+export const db = firebase.firestore();
