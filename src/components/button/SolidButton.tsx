8980c650d3797acfe7ca6000d0a2a688a47e194a
Kai Chang <kaijchang@gmail.com>
2020-07-15 14:41:20 -0700

    make button height match input height

--- src/components/button/SolidButton.tsx
+++ src/components/button/SolidButton.tsx
@@ -5,7 +5,7 @@ import { Button, ButtonProps } from '@chakra-ui/core';
 type SolidButtonProps = ButtonProps;
 
 const SolidButton: React.FunctionComponent<SolidButtonProps> = ({ children, ...props }) => (
-    <Button size='lg' variant='solid' border='none' rounded='full' width='100%' height='57px' { ...props }> 
+    <Button size='lg' variant='solid' border='none' rounded='full' width='100%' height='52px' { ...props }> 
         { children }
     </Button>
 );
