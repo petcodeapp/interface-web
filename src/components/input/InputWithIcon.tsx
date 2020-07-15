72e0124969d730ea1f03743e57b82e5dc2288a3e
Rishi Kothari <itsrishikothari@gmail.com>
2020-07-15 16:52:28 -0400

    update types and spacing

--- src/components/input/InputWithIcon.tsx
+++ src/components/input/InputWithIcon.tsx
@@ -1,18 +1,39 @@
-import React from 'react';
+import React from "react";
 
-import { Icon, Input, InputGroup, InputLeftElement, InputGroupProps, InputProps } from '@chakra-ui/core';
+import {
+  Icon,
+  Input,
+  InputGroup,
+  InputLeftElement,
+  InputGroupProps,
+  InputProps,
+} from "@chakra-ui/core";
 
-type InputWithIconProps = {
-    iconName: string;
-    inputGroupProps?: Omit<InputGroupProps, 'children'>;
-    inputProps?: InputProps;
+interface InputWithIconProps {
+  iconName: string;
+  inputGroupProps?: Omit<InputGroupProps, "children">;
+  inputProps?: InputProps;
 };
 
-const InputWithIcon: React.FunctionComponent<InputWithIconProps> = ({ iconName, inputGroupProps={}, inputProps={} }) => (
-    <InputGroup size='lg' { ...inputGroupProps }>
-        <InputLeftElement children={ <Icon size='25px' name={ iconName } color='petcode.neutral.400'/> }/>
-        <Input rounded='full' color='petcode.neutral.700' borderColor='petcode.neutral.400' focusBorderColor='petcode.neutral.500' { ...inputProps }/>
-    </InputGroup>
+const InputWithIcon: React.FC<InputWithIconProps> = ({
+  iconName,
+  inputGroupProps = {},
+  inputProps = {},
+}) => (
+  <InputGroup size="lg" {...inputGroupProps}>
+    <InputLeftElement
+      children={
+        <Icon size="25px" name={iconName} color="petcode.neutral.400" />
+      }
+    />
+    <Input
+      rounded="full"
+      color="petcode.neutral.700"
+      borderColor="petcode.neutral.400"
+      focusBorderColor="petcode.neutral.500"
+      {...inputProps}
+    />
+  </InputGroup>
 );
 
 export default InputWithIcon;
