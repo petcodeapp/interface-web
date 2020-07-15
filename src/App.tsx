245a09db6d6909bf308bc5357cdaab04aa605af0
Kai Chang <kaijchang@gmail.com>
2020-07-15 13:14:45 -0700

    port over login page

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
 import React from "react";
-import { ThemeProvider, ColorModeProvider, theme } from "@chakra-ui/core";
+import { ThemeProvider, ColorModeProvider } from "@chakra-ui/core";
 import { BrowserRouter as Router, Switch } from "react-router-dom";
 import Routes from "./views/Router";
 import { AuthProvider } from './views/Auth/index';
+import theme from './theme';
+
+import './styles/base.css';
 
 
 function App() {
