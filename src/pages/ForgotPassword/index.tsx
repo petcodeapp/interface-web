ae063b81e9ac19ad55be2734cc67f90cfdeb6b24
Kai Chang <kaijchang@gmail.com>
2020-07-15 17:16:17 -0700

    style digit inputs

--- src/pages/ForgotPassword/index.tsx
+++ src/pages/ForgotPassword/index.tsx
@@ -1,10 +1,31 @@
 import React from 'react';
 
-import { Flex } from '@chakra-ui/core';
+import { Flex, Input } from '@chakra-ui/core';
+
+const DigitInput = () => (
+    <Input
+        backgroundColor='petcode.neutral.200'
+        color='transparent'
+        textShadow='0 0 0 black'
+        borderRadius='34px'
+        height='138px'
+        width='138px'
+        borderColor='none'
+        textAlign='center'
+        fontSize='7xl'
+        fontFamily='Nunito'
+        value=''
+        marginX='14px'
+        padding={0}
+    />
+);
 
 const ForgotPasswordPage = () => (
-    <Flex direction='row' height='100%'>
-        Hello
+    <Flex direction='row' height='100%' alignItems='center' justifyContent='center'>
+        <DigitInput/>
+        <DigitInput/>
+        <DigitInput/>
+        <DigitInput/>
     </Flex>
 );
 
