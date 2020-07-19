8475c3a8f076ac0d43a281f2c25c37a26cee0bca
Kai Chang <kaijchang@gmail.com>
2020-07-19 16:39:32 -0700

    add splash image

--- src/pages/Landing/index.tsx
+++ src/pages/Landing/index.tsx
@@ -1,7 +1,17 @@
 import React from 'react';
 
+import { Box, Flex } from '@chakra-ui/core';
+
 const LandingPage: React.FunctionComponent = () => (
-    <div>Landing</div>
+    <Flex direction='column' minHeight='100%'>
+        <Box
+            backgroundImage='url(/media/karans-dog.JPG)' backgroundPosition='center' backgroundSize='cover'
+            paddingY={250} paddingLeft={100}
+        >
+            <Flex>
+            </Flex>
+        </Box>
+    </Flex>
 );
 
 export default LandingPage;
