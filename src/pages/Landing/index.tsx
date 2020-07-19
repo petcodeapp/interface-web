34e4de048a9d2ca0abd29e8bb9fedab273ba9dac
Kai Chang <kaijchang@gmail.com>
2020-07-19 16:46:01 -0700

    add splash image text

--- src/pages/Landing/index.tsx
+++ src/pages/Landing/index.tsx
@@ -1,14 +1,23 @@
 import React from 'react';
 
-import { Box, Flex } from '@chakra-ui/core';
+import { Box, Flex, Heading, Text } from '@chakra-ui/core';
 
 const LandingPage: React.FunctionComponent = () => (
     <Flex direction='column' minHeight='100%'>
         <Box
             backgroundImage='url(/media/karans-dog.JPG)' backgroundPosition='center' backgroundSize='cover'
-            paddingY={250} paddingLeft={100}
+            paddingY={200} paddingLeft={100}
         >
-            <Flex>
+            <Flex direction='column'>
+                <Heading
+                    color='petcode.yellow' fontSize='6xl'
+                    textShadow='0px 4px 4px rgba(0, 0, 0, 0.25)'
+                >
+                    One Code
+                </Heading>
+                <Text color='white' fontSize='3xl' width='400px'>
+                    An endless suite of features for pet owners.
+                </Text>
             </Flex>
         </Box>
     </Flex>
