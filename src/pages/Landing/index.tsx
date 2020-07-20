2fa12eb1706a5246e022257d7e0b297ea2ecfa91
Kai Chang <kaijchang@gmail.com>
2020-07-19 19:52:36 -0700

    add heading

--- src/pages/Landing/index.tsx
+++ src/pages/Landing/index.tsx
@@ -1,8 +1,37 @@
 import React from 'react';
 
-import { Box, Flex, Heading, Text } from '@chakra-ui/core';
+import { Box, BoxProps, Flex, Heading, Icon, Text } from '@chakra-ui/core';
 
 import SolidButton, { SolidButtonProps } from '../../components/Shared/button/SolidButton';
+
+const HeaderText = (props: BoxProps) => (
+    <Text color='white' fontSize='lg' textTransform='uppercase' { ...props }/>
+);
+
+const Header = () => (
+    <Flex
+        position='fixed'
+        top={0}
+        background='rgba(0, 0, 0, 0.4)'
+        width='calc(100% - 4rem)'
+        paddingX={8}
+        paddingY={4}
+    >
+        <HeaderText>PetCode</HeaderText>
+        <Box flexGrow={1}/>
+        <HeaderText marginRight={8}>Home</HeaderText>
+        <HeaderText marginRight={8}>About Us</HeaderText>
+        <HeaderText marginRight={8}>Purchase</HeaderText>
+        <HeaderText marginRight={8}>
+            <Icon name='template' height='22px' width='22px'/>
+            Register
+        </HeaderText>
+        <HeaderText>
+            <Icon name='user-circle' height='22px' width='22px'/>
+            Sign In
+        </HeaderText>
+    </Flex>
+);
 
 const SplashButton = (props: SolidButtonProps) => (
     <SolidButton
@@ -17,8 +46,11 @@ const SplashButton = (props: SolidButtonProps) => (
 
 const Splash = () => (
     <Box
-        backgroundImage='url(/media/karans-dog.JPG)' backgroundPosition='center' backgroundSize='cover'
-        paddingY={200} paddingLeft={100}
+        backgroundImage='url(/media/karans-dog.JPG)'
+        backgroundPosition='center'
+        backgroundSize='cover'
+        paddingY={200}
+        paddingLeft={100}
     >
         <Flex direction='column'>
             <Heading
@@ -47,6 +79,7 @@ const Splash = () => (
 
 const LandingPage: React.FunctionComponent = () => (
     <Flex direction='column' minHeight='100%'>
+        <Header/>
         <Splash/>
     </Flex>
 );
