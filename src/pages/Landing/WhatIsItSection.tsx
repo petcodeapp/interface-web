b5f46690f057621792f2f193aaaca69b64d12076
Kai Chang <kaijchang@gmail.com>
2020-07-26 14:37:40 -0700

    use useBreakpoint to change alignment of card content responsively

--- src/pages/Landing/WhatIsItSection.tsx
+++ src/pages/Landing/WhatIsItSection.tsx
@@ -1,8 +1,10 @@
 import React from 'react';
 
-import { Flex, FlexProps, Heading, Image, Text } from '@chakra-ui/core';
+import { Flex, FlexProps, Heading, Image, Text, useTheme } from '@chakra-ui/core';
 
 import IconBadge from '../../components/Shared/badge/IconBadge';
+
+import useBreakpoint, { Config } from 'use-breakpoint';
 
 type WhatIsItCardProps = {
     align: 'left'|'center'|'right';
@@ -52,74 +54,82 @@ const WhatIsItCard: React.FC<WhatIsItCardProps> = (
     </Flex>
 );
 
-const WhatIsItSection = () => (
-    <Flex direction='column' alignItems='center' paddingY={12}>
-        <Heading color='petcode.blue.400' fontSize={ { xs: '5xl', md: '6xl' } }>
-            What Is It?
-        </Heading>
-        <Text color='petcode.neutral.500' fontSize={ { xs: 'xl', md: '2xl' } } textAlign='center' marginX={ { xs: 16, md: 64 } } marginY={3}>
-            The PetCode Tag is a simple tag with a QR code that unlocks all the features below and more.
-        </Text>
-        <Flex direction='column' marginX={ { xs: 8, sm: 40, md: 20 } }>
-            <Flex direction={ { xs: 'column', md: 'row' } } alignItems={ { xs: 'center', md: 'start' } }>
-                <WhatIsItCard
-                    align='left'
-                    header='Customizable Pet Profiles'
-                    description='Customize your Pet Profile to match your pet’s behavior, characteristics, dietary needs, and more!'
-                    iconName='template'
-                    iconBgColor='#727DE3'
-                    flexBasis='35%'
-                    marginRight={ { xs: 0, md: 24 } }
-                />
-                <Image display={ { xs: 'none', md: 'block' } } alt='Dog on yellow background' src='/media/dog-on-yellow-background.png' height='300px' width='300px'/>
-                <WhatIsItCard
-                    align='right'
-                    header='Contact Information'
-                    description='Keep all your contact information up to date, so you can be easily reached.'
-                    iconName='messages'
-                    iconBgColor='#4FD1AA'
-                    flexBasis='35%'
-                    marginLeft={ { xs: 0, md: 24 } }
-                    marginTop={ { xs: 6, md: 0 } }
-                />
-            </Flex>
-            <Flex direction={ { xs: 'column', md: 'row' } } alignItems={ { xs: 'center', md: 'start' } }>
-                <WhatIsItCard
-                    align='left'
-                    header='Medical Information'
-                    description='Display all of your pet’s medical information easily, all in one place.'
-                    iconName='clipboard'
-                    iconBgColor='#4299E1'
-                    position='relative'
-                    top={ { md: '-70px' } }
-                    flexBasis='33%'
-                    paddingLeft={ { xs: 0, md: 6 } }
-                    marginTop={ { xs: 6, md: 0 } }
-                />
-                <WhatIsItCard
-                    align='center'
-                    header='Scan Locations'
-                    description='View everywhere your pet’s tag has been scanned. If your pet is lost, a message can be sent out to all PetCode users within a 5 mile radius.'
-                    iconName='flag'
-                    iconBgColor='petcode.blue.400'
-                    flexBasis='33%'
-                    marginTop={6}
-                />
-                <WhatIsItCard
-                    align='right'
-                    header='Integrations'
-                    description='Simplify your pet’s information by using integrations to upload vaccination history and registration.'
-                    iconName='link'
-                    iconBgColor='petcode.teal'
-                    position='relative'
-                    top={ { md: '-70px' } }
-                    flexBasis='33%'
-                    paddingRight={ { xs: 0, md: 6 } }
-                    marginTop={ { xs: 6, md: 0 } }
-                />
+const WhatIsItSection = () => {
+    const theme = useTheme();
+    const { breakpoint } = useBreakpoint(
+        theme.breakpoints as unknown as { [key: string]: number },
+        'xl'
+    );
+
+    return (
+        <Flex direction='column' alignItems='center' paddingY={12}>
+            <Heading color='petcode.blue.400' fontSize={ { xs: '5xl', md: '6xl' } }>
+                What Is It?
+            </Heading>
+            <Text color='petcode.neutral.500' fontSize={ { xs: 'xl', md: '2xl' } } textAlign='center' marginX={ { xs: 16, md: 64 } } marginY={3}>
+                The PetCode Tag is a simple tag with a QR code that unlocks all the features below and more.
+            </Text>
+            <Flex direction='column' marginX={ { xs: 8, sm: 40, md: 20 } }>
+                <Flex direction={ { xs: 'column', md: 'row' } } alignItems={ { xs: 'center', md: 'start' } }>
+                    <WhatIsItCard
+                        align={ breakpoint > 1 ? 'left' : 'center' }
+                        header='Customizable Pet Profiles'
+                        description='Customize your Pet Profile to match your pet’s behavior, characteristics, dietary needs, and more!'
+                        iconName='template'
+                        iconBgColor='#727DE3'
+                        flexBasis='35%'
+                        marginRight={ { xs: 0, md: 24 } }
+                    />
+                    <Image display={ { xs: 'none', md: 'block' } } alt='Dog on yellow background' src='/media/dog-on-yellow-background.png' height='300px' width='300px'/>
+                    <WhatIsItCard
+                        align={ breakpoint > 1 ? 'right' : 'center' }
+                        header='Contact Information'
+                        description='Keep all your contact information up to date, so you can be easily reached.'
+                        iconName='messages'
+                        iconBgColor='#4FD1AA'
+                        flexBasis='35%'
+                        marginLeft={ { xs: 0, md: 24 } }
+                        marginTop={ { xs: 6, md: 0 } }
+                    />
+                </Flex>
+                <Flex direction={ { xs: 'column', md: 'row' } } alignItems={ { xs: 'center', md: 'start' } }>
+                    <WhatIsItCard
+                        align={ breakpoint > 1 ? 'left' : 'center' }
+                        header='Medical Information'
+                        description='Display all of your pet’s medical information easily, all in one place.'
+                        iconName='clipboard'
+                        iconBgColor='#4299E1'
+                        position='relative'
+                        top={ { md: '-70px' } }
+                        flexBasis='33%'
+                        paddingLeft={ { xs: 0, md: 6 } }
+                        marginTop={ { xs: 6, md: 0 } }
+                    />
+                    <WhatIsItCard
+                        align='center'
+                        header='Scan Locations'
+                        description='View everywhere your pet’s tag has been scanned. If your pet is lost, a message can be sent out to all PetCode users within a 5 mile radius.'
+                        iconName='flag'
+                        iconBgColor='petcode.blue.400'
+                        flexBasis='33%'
+                        marginTop={6}
+                    />
+                    <WhatIsItCard
+                        align={ breakpoint > 1 ? 'right' : 'center' }
+                        header='Integrations'
+                        description='Simplify your pet’s information by using integrations to upload vaccination history and registration.'
+                        iconName='link'
+                        iconBgColor='petcode.teal'
+                        position='relative'
+                        top={ { md: '-70px' } }
+                        flexBasis='33%'
+                        paddingRight={ { xs: 0, md: 6 } }
+                        marginTop={ { xs: 6, md: 0 } }
+                    />
+                </Flex>
             </Flex>
         </Flex>
-    </Flex>
-);
+    );
+};
 
 export default WhatIsItSection;
