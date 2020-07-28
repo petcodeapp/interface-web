972065e1d1374005c89d3cf62caff6a58c930515
Kai Chang <kaijchang@gmail.com>
2020-07-28 10:05:50 -0700

    create sidebar

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -3,11 +3,15 @@ import React from 'react';
 import { Flex } from '@chakra-ui/core';
 
 import Header from '../../components/Shared/header';
+import Sidebar from './sections/Sidebar';
 
 const DashboardPage = () => (
     <Flex direction='column' paddingTop='57px'>
         <Header backgroundColor='petcode.neutral.700'/>
-        Dashboard
+        <Flex direction='row'>
+            <Sidebar/>
+            Dashboard
+        </Flex>
     </Flex>
 );
 
