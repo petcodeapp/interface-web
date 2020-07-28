1720def0abac332fd93c0c9a9bce8f18d6140e5b
Kai Chang <kaijchang@gmail.com>
2020-07-28 10:00:51 -0700

    add arguments to testimonial component

--- src/pages/Landing/TestimonialSection.tsx
+++ src/pages/Landing/TestimonialSection.tsx
@@ -2,7 +2,19 @@ import React from 'react';
 
 import { Box, Flex, Heading, Icon, Text } from '@chakra-ui/core';
 
-const Testimonial = () => (
+type TestimonialProps = {
+    numStars: number;
+    testimonialText: string;
+    testifier: string;
+    testifierTitle: string;
+};
+
+const Testimonial: React.FC<TestimonialProps> = ({
+    numStars,
+    testimonialText,
+    testifier,
+    testifierTitle
+}) => (
     <Flex direction={ { xs: 'column', md: 'row' } } alignItems='center' marginX={64}>
         <Box
             rounded='full'
@@ -16,18 +28,18 @@ const Testimonial = () => (
         />
         <Flex direction='column' fontSize='2xl' fontWeight='thin'>
             <Box marginBottom={2}>
-                <Icon name='star' size='30px' color='petcode.yellow.400' marginX={1}/>
-                <Icon name='star' size='30px' color='petcode.yellow.400' marginX={1}/>
-                <Icon name='star' size='30px' color='petcode.yellow.400' marginX={1}/>
-                <Icon name='star' size='30px' color='petcode.yellow.400' marginX={1}/>
-                <Icon name='star' size='30px' color='petcode.yellow.400' marginX={1}/>
+                {
+                    new Array(numStars).fill(null).map((_, idx) => (
+                        <Icon name='star' size='30px' color='petcode.yellow.400' marginX={1}/>
+                    ))
+                }
             </Box>
             <Text color='petcode.neutral.600' marginBottom={6}>
-                “This is an example of a testimonial. When we sell our product, there will be great reviews that go here.”
+                { testimonialText}
             </Text>
             <Flex direction='row' alignItems='center' marginBottom={6}>
-                <Text color='petcode.neutral.700' marginRight={ { md: 8 } }>- Jane Doe</Text>
-                <Text color='petcode.neutral.400'>Pet Owner</Text>
+                <Text color='petcode.neutral.700' marginRight={ { md: 8 } }>- { testifier }</Text>
+                <Text color='petcode.neutral.400'>{ testifierTitle }</Text>
                 <Box flexGrow={1}/>
                 <Icon name='arrow' color='petcode.neutral.400' size='50px'/>
             </Flex>
@@ -40,7 +52,12 @@ const TestimonialSection = () => (
         <Heading color='petcode.blue.400' fontSize={ { xs: '5xl', md: '6xl' } } marginBottom={12}>
             Owner Testimonials
         </Heading>
-        <Testimonial/>
+        <Testimonial
+            numStars={5}
+            testimonialText='“This is an example of a testimonial. When we sell our product, there will be great reviews that go here.”'
+            testifier='Jane Doe'
+            testifierTitle='Pet Owner'
+        />
     </Flex>
 );
 
