5213a6550981d57e1033a6b5f751244df0854d48
Kai Chang <kaijchang@gmail.com>
2020-07-31 12:54:10 -0700

    add link cards

--- src/pages/Account/PetInfoPage.tsx
+++ src/pages/Account/PetInfoPage.tsx
@@ -1,25 +1,26 @@
 import React from 'react';
 
-import { Box, Flex, Icon, Text } from '@chakra-ui/core';
+import { Box, Flex, Icon, Link, Text } from '@chakra-ui/core';
+import { Link as RouterLink } from 'react-router-dom';
 
 import AccountPageLayout from './components/AccountPageLayout';
 
 const PetInfoSection = () => (
     <Flex direction='column' flexGrow={1} backgroundColor='petcode.neutral.200' padding={10}>
-        <Flex direction='row' alignItems='center'>
+        <Flex direction='row' alignItems='center' color='petcode.neutral.700'>
             <Box
                 rounded='full'
                 size='150px'
+                minWidth='150px'
                 backgroundSize='cover'
                 backgroundPosition='center'
                 backgroundImage='url(/media/placeholder-dog.png)'
             />
-            <Flex height='100px' alignItems='center' marginLeft={4}>
-                <Icon position='absolute' size='120px' transform='rotate(12.84deg)' color='petcode.neutral.300' name='paw'/>
+            <Flex alignItems='center' marginLeft={4}>
+                <Icon position='absolute' size='120px' transform='rotate(12.84deg)' opacity={0.05} name='paw'/>
                 <Text
                     position='relative'
                     zIndex={2}
-                    color='petcode.neutral.700'
                     fontSize='6xl'
                     marginLeft={16}
                 >
@@ -27,6 +28,41 @@ const PetInfoSection = () => (
                 </Text>
             </Flex>
             <Box flexGrow={1}/>
+            <Link
+                // @ts-ignore
+                as={ RouterLink }
+                to='/contactinfo'
+                display='flex'
+                flexDirection='column'
+                justifyContent='center'
+                rounded='lg'
+                backgroundColor='petcode.yellow.400'
+                flexBasis='200px'
+                padding={6}
+                marginX={6}
+            >
+                <Text position='relative' fontSize='3xl' zIndex={2}>
+                    Contact Information
+                </Text>
+                <Icon position='absolute' alignSelf='end' size='100px' opacity={0.05} name='phone'/>
+            </Link>
+            <Link
+                // @ts-ignore
+                as={ RouterLink }
+                to='/medicalinfo'
+                display='flex'
+                flexDirection='column'
+                justifyContent='center'
+                rounded='lg'
+                backgroundColor='petcode.yellow.400'
+                flexBasis='200px'
+                padding={6}
+            >
+                <Text position='relative' fontSize='3xl' zIndex={2}>
+                    Medical Information
+                </Text>
+                <Icon position='absolute' alignSelf='end' size='100px' opacity={0.05} name='clipboard'/>
+            </Link>
         </Flex>
     </Flex>
 );
