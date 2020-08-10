bdda467b34b9f72ac68f1cee45699bb568be7783
Rishi Kothari <itsrishikothari@gmail.com>
2020-08-10 08:41:48 -0400

    admin

--- src/pages/Admin/index.tsx
+++ src/pages/Admin/index.tsx
@@ -1,21 +1,16 @@
-import React, { useContext } from 'react';
-import { useObserver } from 'mobx-react';
-import { AuthContext } from '../../views/Auth/index';
+import React, { useContext } from "react";
+import { useObserver } from "mobx-react";
+import { AuthContext } from "../../views/Auth/index";
 
 const AdminPage = () => {
-    const auth = useContext(AuthContext);
+  const auth = useContext(AuthContext);
 
-    return useObserver(() => (
-        <>
-            <div>Admin Page</div>
-            {
-                auth.isLoggedIn && (
-                    <button onClick={auth.signOut}>Sign out!</button>
-                )
-            }
-
-        </>
-    ))
-}
+  return useObserver(() => (
+    <>
+      <div>Admin Page</div>
+      {auth.isLoggedIn && <button onClick={auth.signOut}>Sign out!</button>}
+    </>
+  ));
+};
 
 export default AdminPage;
