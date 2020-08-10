14446234fb7d8eaf2b4a6a38cd5749037817e216
Rishi Kothari <itsrishikothari@gmail.com>
2020-08-10 08:41:02 -0400

    app testing

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,10 @@ import React from "react";
 import { ThemeProvider, ColorModeProvider } from "@chakra-ui/core";
 import { BrowserRouter as Router, Switch } from "react-router-dom";
 import Routes from "./views/Router";
-import { AuthProvider } from './views/Auth/index';
-import theme from './theme';
+import { AuthProvider } from "./views/Auth/index";
+import theme from "./theme";
 
-import './styles/base.css';
-
+import "./styles/base.css";
 
 function App() {
   return (
