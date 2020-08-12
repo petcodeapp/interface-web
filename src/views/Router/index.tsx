aecb69aff41e1e8d49a7f4137b796702f45e3406
Rishi Kothari <itsrishikothari@gmail.com>
2020-08-12 10:37:21 -0400

    motion divs

--- src/views/Router/index.tsx
+++ src/views/Router/index.tsx
@@ -14,58 +14,95 @@ import RemindersPage from "../../pages/Account/RemindersPage";
 import MedicalInfoPage from "../../pages/Account/MedicalInfoPage";
 import ScanLocationsPage from "../../pages/Account/ScanLocationsPage";
 import { useObserver } from "mobx-react-lite";
+import { motion } from "framer-motion";
+
+const pageVariants = {
+  initial: {
+    opacity: 0,
+  },
+  in: {
+    opacity: 1,
+  },
+  out: {
+    opacity: 0,
+  },
+}
+
 
 const Routes = () => {
   return useObserver(() => (
     <>
       <PublicRoute restricted path="/signup">
+        <motion.div initial="initial" animate="in" exit="out" variants={pageVariants}>
         <RegistrationPage />
+        </motion.div>
       </PublicRoute>
 
       <PublicRoute restricted path="/login">
+      <motion.div initial="initial" animate="in" exit="out" variants={pageVariants}>
         <LoginPage />
+        </motion.div>
       </PublicRoute>
 
       <PublicRoute restricted path="/forgotpassword">
+      <motion.div initial="initial" animate="in" exit="out" variants={pageVariants}>
         <ForgotPasswordPage />
+        </motion.div>
       </PublicRoute>
 
       <PrivateRoute path="/admin">
+      <motion.div initial="initial" animate="in" exit="out" variants={pageVariants}>
         <AdminPage />
+        </motion.div>
       </PrivateRoute>
 
       <PrivateRoute path="/dashboard">
+      <motion.div initial="initial" animate="in" exit="out" variants={pageVariants}>
         <DashboardPage />
+        </motion.div>
       </PrivateRoute>
 
       <PrivateRoute path="/contactinfo">
+      <motion.div initial="initial" animate="in" exit="out" variants={pageVariants}>
         <ContactInfoPage />
+        </motion.div>
       </PrivateRoute>
 
       <PrivateRoute path="/petinfo">
+      <motion.div initial="initial" animate="in" exit="out" variants={pageVariants}>
         <PetInfoPage />
+        </motion.div>
       </PrivateRoute>
 
       <PrivateRoute path="/reminders">
+      <motion.div initial="initial" animate="in" exit="out" variants={pageVariants}>
         <RemindersPage />
+        </motion.div>
       </PrivateRoute>
 
       <PrivateRoute path="/medicalinfo">
+      <motion.div initial="initial" animate="in" exit="out" variants={pageVariants}>
         <MedicalInfoPage />
+        </motion.div>
       </PrivateRoute>
 
       <PrivateRoute path="/scanlocations">
-        <ScanLocationsPage />
+      <motion.div initial="initial" animate="in" exit="out" variants={pageVariants}>
+        <ScanLocationsPage /></motion.div>
       </PrivateRoute>
 
       <PublicRoute path="/oldlanding">
+      <motion.div initial="initial" animate="in" exit="out" variants={pageVariants}>
         <OldLandingpage />
+        </motion.div>
       </PublicRoute>
 
       <PublicRoute path="/about">{/* TODO: Create about page */}</PublicRoute>
 
       <PublicRoute exact path="/">
+      <motion.div initial="initial" animate="in" exit="out" variants={pageVariants}>
         <LandingPage />
+        </motion.div>
       </PublicRoute>
     </>
   ));
