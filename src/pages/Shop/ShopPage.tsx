86ee85a38f2de75e31875120da79b4404e48ce76
Kai Chang <kaijchang@gmail.com>
2020-08-15 14:09:13 -0700

    product model

--- src/pages/Shop/ShopPage.tsx
+++ src/pages/Shop/ShopPage.tsx
@@ -4,51 +4,72 @@ import {
   Box,
   Flex,
   Heading,
+  Link,
   Stack,
   StackProps,
   Text,
 } from "@chakra-ui/core";
 
+import { Link as RouterLink } from "react-router-dom";
+
 import Rating from "../../components/Shared/rating";
 import Layout from "../../components/Shared/layout";
 
-const ShopItem: React.FC<StackProps> = (props) => (
-  <Stack width="250px" spacing={3} {...props}>
-    <Box size="250px" backgroundColor="petcode.neutral.300" />
-    <Text color="petcode.neutral.600" fontSize="xl" fontWeight="thin">
-      PetCode Tag
-    </Text>
-    <Flex direction="row" justifyContent="space-between">
-      <Text color="petcode.neutral.500" fontSize="lg" fontWeight="thin">
-        $20.00
+import { Product } from "../../Models/Product";
+
+const ShopItem: React.FC<StackProps & { product: Product }> = ({ product, ...props }) => (
+  <Link
+    // @ts-ignore
+    as={RouterLink}
+    to={`/products/${product.id}`}
+  >
+    <Stack width="250px" spacing={3} {...props}>
+      <Box size="250px" backgroundColor="petcode.neutral.300" />
+      <Text color="petcode.neutral.600" fontSize="xl" fontWeight="thin">
+        {product.name}
       </Text>
-      <Rating
-        size="lg"
-        rating={5}
-      />
-    </Flex>
-  </Stack>
+      <Flex direction="row" justifyContent="space-between">
+        <Text color="petcode.neutral.500" fontSize="lg" fontWeight="thin">
+          ${product.price.toFixed(2)}
+        </Text>
+        <Rating
+          size="lg"
+          rating={product.averageRating}
+        />
+      </Flex>
+    </Stack>
+  </Link>
 );
 
-const ShopPage = () => (
-  <Layout>
-    <Stack spacing={6} padding={12}>
-      <Heading fontSize="5xl" color="petcode.blue.400">
-        All Products
-      </Heading>
-      <Stack isInline flexWrap="wrap" spacing={10}>
-        <ShopItem marginBottom={6} />
-        <ShopItem marginBottom={6} />
-        <ShopItem marginBottom={6} />
-        <ShopItem marginBottom={6} />
-        <ShopItem marginBottom={6} />
-        <ShopItem marginBottom={6} />
-        <ShopItem marginBottom={6} />
-        <ShopItem marginBottom={6} />
-        <ShopItem marginBottom={6} />
+const ShopPage = () => {
+  const products = Array(9).fill(null).map((_, idx) => ({
+    id: String(idx),
+    name: "PetCode Tag",
+    price: 20,
+    imageURL: "",
+    averageRating: 5,
+    description: "",
+    longDescription: ""
+  })) as Product[];
+
+  return (
+    <Layout>
+      <Stack spacing={6} padding={12}>
+        <Heading fontSize="5xl" color="petcode.blue.400">
+          All Products
+        </Heading>
+        <Stack isInline flexWrap="wrap" spacing={10}>
+          {products.map((product, idx) => (
+            <ShopItem
+              key={idx}
+              product={product}
+              marginBottom={6}
+            />
+          ))}
+        </Stack>
       </Stack>
-    </Stack>
-  </Layout>
-);
+    </Layout>
+  );
+};
 
 export default ShopPage;
