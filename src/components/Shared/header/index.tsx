acf143820bbbc51f3711c0fa2efa39bf4b061d9b
Kai Chang <kaijchang@gmail.com>
2020-08-29 15:13:57 -0700

    new header

--- src/components/Shared/header/index.tsx
+++ src/components/Shared/header/index.tsx
@@ -1,8 +1,23 @@
 import React from "react";
 
-import { Box, Flex, FlexProps, Icon, Text } from "@chakra-ui/core";
+import { Box, BoxProps, Flex, FlexProps, Image, Text } from "@chakra-ui/core";
 import { useObserver } from "mobx-react-lite";
 import { AuthContext } from "../../../views/Auth/index";
+
+import BaseButton, { BaseButtonProps } from "../button/BaseButton";
+
+const HeaderText: React.FC<BoxProps> = (props) => (
+  <Text
+    fontSize="lg"
+    textTransform="uppercase"
+    fontFamily="Open Sans"
+    {...props}
+  />
+);
+
+const HeaderButton: React.FC<BaseButtonProps> = (props) => (
+  <BaseButton size="sm" variantColor="petcode.yellow" paddingY={4} {...props} />
+);
 
 const Header: React.FC<FlexProps> = (props) => {
   const auth = React.useContext(AuthContext);
@@ -10,33 +25,41 @@ const Header: React.FC<FlexProps> = (props) => {
   return useObserver(() => (
     <Flex
       position="fixed"
+      alignItems="center"
       top={0}
       background="rgba(0, 0, 0, 0.4)"
       width="calc(100% - 4rem)"
       paddingX={8}
-      paddingY={4}
+      paddingY={2}
       zIndex={999}
       color="white"
-      fontSize="lg"
-      textTransform="uppercase"
+      fontWeight="bold"
       {...props}
     >
-      <Text>PetCode</Text>
+      <Image src="/media/petcode-logo-with-qr-code.png" height="40px" />
       <Box flexGrow={1} />
-      <Text marginRight={8}>Home</Text>
-      <Text marginRight={8}>About Us</Text>
-      <Text marginRight={8}>Purchase</Text>
-      {!auth.isLoggedIn && (
+      <HeaderText marginRight={8}>Home</HeaderText>
+      <HeaderText marginRight={8}>About Us</HeaderText>
+      <HeaderText marginRight={8}>Purchase</HeaderText>
+      {!auth.isLoggedIn ? (
         <>
-          <Text display="flex" alignItems="center" marginRight={8}>
-            <Icon name="template" size="15px" marginRight={2} />
-            Register
-          </Text>
-          <Text display="flex" alignItems="center">
-            <Icon name="user-circle" size="16px" marginRight={2} />
-            Sign In
-          </Text>
+          <HeaderButton>
+            <HeaderText fontWeight="thin" color="petcode.neutral.700">
+              Register
+            </HeaderText>
+          </HeaderButton>
+          <HeaderButton>
+            <HeaderText fontWeight="thin" color="petcode.neutral.700">
+              Sign In
+            </HeaderText>
+          </HeaderButton>
         </>
+      ) : (
+        <HeaderButton>
+          <HeaderText fontWeight="thin" color="petcode.neutral.700">
+            Dashboard
+          </HeaderText>
+        </HeaderButton>
       )}
     </Flex>
   ));
