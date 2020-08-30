be0ec7cfd6273f7da3d5eb20e0ed7be18c75e6fc
Kai Chang <kaijchang@gmail.com>
2020-08-30 14:13:33 -0700

    prettier

--- src/components/Shared/header/index.tsx
+++ src/components/Shared/header/index.tsx
@@ -1,6 +1,14 @@
 import React from "react";
 
-import { Box, Flex, FlexProps, Image, Link, LinkProps, Stack } from "@chakra-ui/core";
+import {
+  Box,
+  Flex,
+  FlexProps,
+  Image,
+  Link,
+  LinkProps,
+  Stack,
+} from "@chakra-ui/core";
 import { useObserver } from "mobx-react-lite";
 import { AuthContext } from "../../../views/Auth/index";
 
@@ -40,25 +48,37 @@ const Header: React.FC<FlexProps> = (props) => {
     >
       <Image src="/media/petcode-logo-with-qr-code.png" height="40px" />
       <Box flexGrow={1} />
-      <HeaderLink href='/'>Home</HeaderLink>
+      <HeaderLink href="/">Home</HeaderLink>
       <HeaderLink>About Us</HeaderLink>
-      <HeaderLink href='/products'>Purchase</HeaderLink>
+      <HeaderLink href="/products">Purchase</HeaderLink>
       {!auth.isLoggedIn ? (
         <Stack isInline spacing={4}>
           <HeaderButton>
-            <HeaderLink href="/signup" fontWeight="thin" color="petcode.neutral.700">
+            <HeaderLink
+              href="/signup"
+              fontWeight="thin"
+              color="petcode.neutral.700"
+            >
               Register
             </HeaderLink>
           </HeaderButton>
           <HeaderButton>
-            <HeaderLink href="/login" fontWeight="thin" color="petcode.neutral.700">
+            <HeaderLink
+              href="/login"
+              fontWeight="thin"
+              color="petcode.neutral.700"
+            >
               Sign In
             </HeaderLink>
           </HeaderButton>
         </Stack>
       ) : (
         <HeaderButton>
-          <HeaderLink href="/dashboard" fontWeight="thin" color="petcode.neutral.700">
+          <HeaderLink
+            href="/dashboard"
+            fontWeight="thin"
+            color="petcode.neutral.700"
+          >
             Dashboard
           </HeaderLink>
         </HeaderButton>
