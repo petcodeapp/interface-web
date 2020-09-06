ca08420b99e4d7756996807bbdd46bc6e2b30fc0
Kai Chang <kaijchang@gmail.com>
2020-09-06 14:32:46 -0700

    remove old landing page

--- src/pages/Landing/index.tsx
+++ src/pages/Landing/index.tsx
@@ -1,26 +1,17 @@
 import React from "react";
 
-import { Flex } from "@chakra-ui/core";
+import { Box } from "@chakra-ui/core";
 
-import Header from "../../components/Shared/header";
-
-import Splash from "./Splash";
-import WhatIsItSection from "./WhatIsItSection";
-import WhyPetCodeSection from "./WhyPetCodeSection";
-import GetStartedSection from "./GetStartedSection";
-import TestimonialSection from "./TestimonialSection";
-import Footer from "./Footer";
+import Layout from "../../components/Shared/layout";
 
 const LandingPage: React.FunctionComponent = () => (
-  <Flex direction="column">
-    <Header />
-    <Splash />
-    <WhatIsItSection />
-    <WhyPetCodeSection />
-    <GetStartedSection />
-    <TestimonialSection />
-    <Footer />
-  </Flex>
+  <Layout>
+    <Box>
+      <svg style={{ display: "block" }} viewBox="0 0 1440 915" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <path d="M581.187 68.5299C1040 -37 1446.77 -2.47391 1617 54.516L1486.92 765.722C1009.97 894.65 568.628 860.882 224 827.383C-68 799 -134.128 858.01 -208 915L-339 121.783C-207.315 165.693 147.112 168.37 581.187 68.5299Z" fill="#51BCDA"/>
+      </svg>
+    </Box>
+  </Layout>
 );
 
 export default LandingPage;
