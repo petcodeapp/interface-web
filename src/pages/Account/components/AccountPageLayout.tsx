43332dc5c39a72e4623f108511ebdd41e4393b7d
Rishi Kothari <itsrishikothari@gmail.com>
2020-09-07 15:10:57 -0400

    acct page

--- src/pages/Account/components/AccountPageLayout.tsx
+++ src/pages/Account/components/AccountPageLayout.tsx
@@ -4,15 +4,18 @@ import { Flex } from "@chakra-ui/core";
 
 import Header from "../../../components/Shared/header";
 import Sidebar from "./Sidebar";
+import { motion } from 'framer-motion';
 
-const AccountPageLayout: React.FC = ({ children }) => (
-  <Flex direction="column" minHeight="calc(100% - 57px)" paddingTop="57px">
+const F = motion.custom(Flex)
+
+const AccountPageLayout: React.FC<any> = ({ children, variants }) => (
+  <F variants={variants} direction="column" minHeight="calc(100% - 57px)" paddingTop="57px">
     <Header backgroundColor="petcode.neutral.700" />
     <Flex direction="row" flexGrow={1}>
       <Sidebar />
       {children}
     </Flex>
-  </Flex>
+  </F>
 );
 
 export default AccountPageLayout;
