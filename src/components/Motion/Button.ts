68f575c4f12516fba86fc497693f8cb81d509450
Rishi Kothari <itsrishikothari@gmail.com>
2020-09-22 09:09:58 -0400

    moion

--- src/components/Motion/Button.ts
+++ src/components/Motion/Button.ts
@@ -1,6 +1,6 @@
-import { motion } from "framer-motion";
-import { Button as B } from "@chakra-ui/core";
+import { motion } from 'framer-motion'
+import { Button as B } from '@chakra-ui/core'
 
-const Button = motion.custom(B);
+const Button = motion.custom(B)
 
-export default Button;
+export default Button
