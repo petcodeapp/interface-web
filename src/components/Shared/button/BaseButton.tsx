860a03ac88eeafa0fefe0f769c8810342cac2557
Rishi Kothari <itsrishikothari@gmail.com>
2020-09-22 09:10:02 -0400

    base

--- src/components/Shared/button/BaseButton.tsx
+++ src/components/Shared/button/BaseButton.tsx
@@ -1,31 +1,30 @@
-import React from "react";
+import React from 'react'
 
-import { ButtonProps } from "@chakra-ui/core";
-import { MotionProps } from "framer-motion";
-import Button from "../../Motion/Button";
+import { ButtonProps } from '@chakra-ui/core'
+import { MotionProps } from 'framer-motion'
+import Button from '../../Motion/Button'
 
-export type BaseButtonProps = ButtonProps & MotionProps;
+export type BaseButtonProps = ButtonProps & MotionProps
 
 const BaseButton: React.FunctionComponent<BaseButtonProps> = ({
-  children,
-  variant = "solid",
-  ...props
+	children,
+	variant = 'solid',
+	...props
 }) => (
-  <Button
-    size="lg"
-    variant={variant}
-    rounded="full"
-    whileHover={{
-      scale: 1.05,
-      cursor: "pointer",
-    }}
-    transition={{
-      duration: "0.2",
-    }}
-    {...props}
-  >
-    {children}
-  </Button>
-);
+	<Button
+		size="lg"
+		variant={variant}
+		rounded="full"
+		whileHover={{
+			scale: 1.05,
+			cursor: 'pointer',
+		}}
+		transition={{
+			duration: '0.2',
+		}}
+		{...props}>
+		{children}
+	</Button>
+)
 
-export default BaseButton;
+export default BaseButton
