58d47be379fd6c928d5aa3bcd05e499cfedcd180
Rishi Kothari <itsrishikothari@gmail.com>
2020-09-22 09:11:17 -0400

    webpack

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@
 const path = require('path');
 const webpack = require('webpack');
 const DashboardPlugin = require('webpack-dashboard/plugin');
+const { CheckerPlugin } = require('awesome-typescript-loader')
 
 module.exports = {
 	mode: 'development',
@@ -9,17 +10,19 @@ module.exports = {
 	entry: ['./src/index.tsx'],
 	devtool: 'inline-source-map',
 	module: {
+        
 		rules: [
+            {
+				test: /\.tsx?$/,
+                loader: "babel-loader",
+				exclude: /node_modules/,
+			},
 			{
 				test: /\.(js)$/,
 				exclude: /node_modules/,
 				use: ['babel-loader'],
 			},
-			{
-				test: /\.tsx?$/,
-				use: 'ts-loader',
-				exclude: /node_modules/,
-			},
+			
 		],
 	},
 	resolve: {
@@ -33,7 +36,8 @@ module.exports = {
 	plugins: [
 		new webpack.DefinePlugin({
 			__DEV__: true,
-		}),
+        }),
+        new CheckerPlugin(),
 		new DashboardPlugin(),
 	],
 	devServer: {
