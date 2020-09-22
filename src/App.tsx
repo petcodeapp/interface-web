0a0d817791e5407b6066117276bc4ff1fe9b12cf
Rishi Kothari <itsrishikothari@gmail.com>
2020-09-22 09:09:46 -0400

    app

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,29 @@
-import React from "react";
-import { ThemeProvider, ColorModeProvider } from "@chakra-ui/core";
-import { BrowserRouter as Router, Switch } from "react-router-dom";
-import Routes from "./views/Router";
-import { AuthProvider } from "./views/Auth/index";
-import theme from "./theme";  
-import { AnimatePresence } from "framer-motion";
+import React from 'react'
+import { ThemeProvider, ColorModeProvider } from '@chakra-ui/core'
+import { BrowserRouter as Router, Switch } from 'react-router-dom'
+import { AnimatePresence } from 'framer-motion'
+import Routes from './views/Router'
+import { AuthProvider } from './views/Auth/index'
+import theme from './theme'
 
 function App() {
-  return (
-    <>
-      <ThemeProvider theme={theme}>
-        <ColorModeProvider>
-          <AuthProvider>
-            <AnimatePresence>
-            <Router>
-              <Switch>
-                <Routes />
-              </Switch>
-            </Router>
-            </AnimatePresence>
-          </AuthProvider>
-        </ColorModeProvider>
-      </ThemeProvider>
-    </>
-  );
+	return (
+		<>
+			<ThemeProvider theme={theme}>
+				<ColorModeProvider>
+					<AuthProvider>
+						<AnimatePresence>
+							<Router>
+								<Switch>
+									<Routes />
+								</Switch>
+							</Router>
+						</AnimatePresence>
+					</AuthProvider>
+				</ColorModeProvider>
+			</ThemeProvider>
+		</>
+	)
 }
 
-export default App;
+export default App
