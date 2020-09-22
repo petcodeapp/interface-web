371bedafdd3f442b4342e0e02bbe55b2277d0aa6
Rishi Kothari <itsrishikothari@gmail.com>
2020-09-22 09:10:08 -0400

    evn

--- src/environment/environment.example.ts
+++ src/environment/environment.example.ts
@@ -1,14 +1,14 @@
-interface environment {
-  firebaseConfig: {
-    apiKey: string;
-    authDomain: string;
-    databaseURL: string;
-    projectId: string;
-    storageBucket: string;
-    messagingSenderId: string;
-    appId: string;
-    measurementId: string;
-  };
+interface Environment {
+	firebaseConfig: {
+		apiKey: string
+		authDomain: string
+		databaseURL: string
+		projectId: string
+		storageBucket: string
+		messagingSenderId: string
+		appId: string
+		measurementId: string
+	}
 }
 
-export default environment;
+export default Environment
