09a299ba9c54b0cda7d2b7e72a12527ecfffbc10
Rishi Kothari <itsrishikothari@gmail.com>
2020-09-22 09:10:29 -0400

    ck

--- src/pages/Account/components/Checkbox.tsx
+++ src/pages/Account/components/Checkbox.tsx
@@ -1,33 +1,32 @@
-import React from "react";
+import React from 'react'
 
-import { Flex, FlexProps, Icon } from "@chakra-ui/core";
+import { Flex, FlexProps, Icon } from '@chakra-ui/core'
 
 type CheckboxProps = {
-  checked?: boolean;
-  size?: number;
-} & FlexProps;
+	checked?: boolean
+	size?: number
+} & FlexProps
 
 const Checkbox: React.FC<CheckboxProps> = ({
-  checked,
-  size = 32,
-  ...props
+	checked,
+	size = 32,
+	...props
 }) => (
-  <Flex
-    alignItems="center"
-    justifyContent="center"
-    rounded="full"
-    backgroundColor="petcode.yellow.400"
-    size={size + "px"}
-    {...props}
-  >
-    {checked && (
-      <Icon
-        name="checkmark"
-        size={size - 12 + "px"}
-        color="petcode.neutral.700"
-      />
-    )}
-  </Flex>
-);
+	<Flex
+		alignItems="center"
+		justifyContent="center"
+		rounded="full"
+		backgroundColor="petcode.yellow.400"
+		size={`${size  }px`}
+		{...props}>
+		{checked && (
+			<Icon
+				name="checkmark"
+				size={`${size - 12  }px`}
+				color="petcode.neutral.700"
+			/>
+		)}
+	</Flex>
+)
 
-export default Checkbox;
+export default Checkbox
