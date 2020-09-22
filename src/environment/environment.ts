cc60b18207c932898c6fe5af30c1d3534b46c0ff
Rishi Kothari <itsrishikothari@gmail.com>
2020-09-22 09:20:42 -0400

    env

---
+++ src/environment/environment.ts
@@ -0,0 +1,14 @@
+const environment = {
+	firebaseConfig: {
+		apiKey: 'AIzaSyAoHlfhxjCJsF45xPFfNHbG3bWQsuOHJXQ',
+		authDomain: 'petcode-b1e6e.firebaseapp.com',
+		databaseURL: 'https://petcode-b1e6e.firebaseio.com',
+		projectId: 'petcode-b1e6e',
+		storageBucket: 'petcode-b1e6e.appspot.com',
+		messagingSenderId: '858374127167',
+		appId: '1:858374127167:web:6b0270a202b7a666ad828a',
+		measurementId: 'G-RHSZE35L55',
+	},
+}
+
+export default environment
