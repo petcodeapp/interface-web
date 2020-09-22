d935466390133038d505830af183304544f7c514
Rishi Kothari <itsrishikothari@gmail.com>
2020-09-22 09:11:10 -0400

    router

--- src/views/Router/PrivateRoute.tsx
+++ src/views/Router/PrivateRoute.tsx
@@ -1,13 +1,13 @@
-import React, { useContext } from "react";
-import { Redirect, Route, RouteProps } from "react-router-dom";
-import { AuthContext } from "../Auth";
+import React, { useContext } from 'react'
+import { Redirect, Route, RouteProps } from 'react-router-dom'
+import { AuthContext } from '../Auth'
 
 const PrivateRoute = ({ children, ...props }: RouteProps) => {
-  const auth = useContext(AuthContext);
+	const auth = useContext(AuthContext)
 
-  return (
-    <Route {...props}>{auth.isLoggedIn ? children : <Redirect to="/" />}</Route>
-  );
-};
+	return (
+		<Route {...props}>{auth.isLoggedIn ? children : <Redirect to="/" />}</Route>
+	)
+}
 
-export default PrivateRoute;
+export default PrivateRoute
