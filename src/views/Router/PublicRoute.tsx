784c7507d369a7f3fbad497ff505a9a89fe46386
Rishi Kothari <itsrishikothari@gmail.com>
2020-09-22 09:11:12 -0400

    router

--- src/views/Router/PublicRoute.tsx
+++ src/views/Router/PublicRoute.tsx
@@ -1,25 +1,24 @@
-import { RouteProps, Route, Redirect } from "react-router-dom";
-import React from "react";
-import { AuthContext } from "../Auth";
-import { useObserver } from "mobx-react-lite";
-import { auth } from "../../firebase/index";
+import { RouteProps, Route, Redirect } from 'react-router-dom'
+import React from 'react'
+import { useObserver } from 'mobx-react-lite'
+import { AuthContext } from '../Auth'
 
 interface PublicRouteProps {
-  restricted?: boolean;
+	restricted?: boolean
 }
 
 const PublicRoute = ({
-  children,
-  restricted = false,
-  ...props
+	children,
+	restricted = false,
+	...props
 }: RouteProps & PublicRouteProps) => {
-  const auth = React.useContext(AuthContext);
+	const auth = React.useContext(AuthContext)
 
-  return useObserver(() => (
-    <Route {...props}>
-      {auth.isLoggedIn ? <Redirect to="/dashboard" /> : children}
-    </Route>
-  ));
-};
+	return useObserver(() => (
+		<Route {...props}>
+			{auth.isLoggedIn ? <Redirect to="/dashboard" /> : children}
+		</Route>
+	))
+}
 
-export default PublicRoute;
+export default PublicRoute
