c4c53991c1864e6956703688cb6d7561c1c57165
Kai Chang <kaijchang@gmail.com>
2020-10-01 19:42:07 -0700

    redesign of footer

--- src/components/Shared/organisms/Footer/index.tsx
+++ src/components/Shared/organisms/Footer/index.tsx
@@ -1,88 +1,97 @@
 import React from "react";
 
-import { Box, Divider, Flex, Icon, Image, Text } from "@chakra-ui/core";
+import { Flex, IconButton, Image, Stack, Text } from "@chakra-ui/core";
 
-const Footer = () => (
-  <Flex
-    direction="column"
-    backgroundColor="petcode.neutral.700"
-    paddingX={{ xs: 6, md: 40 }}
-    paddingY={10}
-  >
-    <Flex direction={{ xs: "column", md: "row" }} alignItems="start">
-      <Image
-        alt="PetCode Logo with QR Code"
-        src="/media/petcode-logo-with-qr-code.png"
-        height="45px"
-        marginBottom={{ xs: 4, md: 0 }}
-      />
-      <Flex
-        direction="row"
-        wrap="wrap"
-        fontSize="sm"
-        color="white"
-        fontWeight="thin"
-        textTransform="uppercase"
-        marginX={{ md: 6 }}
-        marginBottom={{ xs: 4, md: 0 }}
-      >
-        <Text flexBasis="33%" marginBottom={2}>
-          Home
-        </Text>
-        <Text flexBasis="33%" marginBottom={2}>
-          Purchase
-        </Text>
-        <Text flexBasis="33%" marginBottom={2}>
-          FAQs
-        </Text>
-        <Text flexBasis="33%" marginBottom={2}>
-          About Us
-        </Text>
-        <Text flexBasis="33%" marginBottom={2}>
-          Register
-        </Text>
-        <Text flexBasis="33%" marginBottom={2}>
-          Shipping and Returns
-        </Text>
-        <Text flexBasis="33%" marginBottom={2}>
-          Contact Us
-        </Text>
-        <Text flexBasis="33%" marginBottom={2}>
-          Login
-        </Text>
-      </Flex>
-      <Flex direction="row" alignItems="center">
-        <Box
-          color="white"
-          rounded="full"
-          padding={4}
-          backgroundColor="#125687"
-          marginRight={3}
-        >
-          <Icon name="instagram" size="20px" />
-        </Box>
-        <Box color="white" rounded="full" padding={4} backgroundColor="#6077A9">
-          <Icon name="facebook" size="20px" />
-        </Box>
-      </Flex>
-    </Flex>
-    <Divider
-      orientation="horizontal"
-      border="1px solid"
-      borderColor="white"
-      width="100%"
-    />
+const SocialMediaButtonStyle = {
+  size: "md",
+  isRound: true,
+  backgroundColor: "white",
+  color: "petcode.blue.400",
+};
+
+const Footer = () => {
+  return (
     <Flex
       direction="row"
       justifyContent="space-between"
-      fontSize="xs"
-      fontWeight="thin"
+      paddingX={40}
       color="white"
+      position="relative"
+      minHeight="calc(100vw * 0.21)"
+      boxSizing="border-box"
+      paddingTop={`${109 / 1440 * 100}%`}
+      paddingBottom={16}
     >
-      <Text>© 2020 PetCode</Text>
-      <Text>Terms and Conditions | Privacy Policy</Text>
+      <svg
+        style={{ position: "absolute", top: 0, left: 0, zIndex: -1 }}
+        viewBox="0 0 1440 309"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path d="M476.657 0.531032C327.339 -1.0681 96.0032 9.19299 -1 14.5234V308.364H1440V26.5169C1153 102.476 663.305 2.52995 476.657 0.531032Z" fill="#B9E4F0"/>
+      </svg>
+      <svg
+        style={{ position: "absolute", top: 0, left: 0, zIndex: -1 }}
+        viewBox="0 0 1440 311"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path d="M911 38.3637C479 -31.7662 211.5 10.3637 -0.5 38.3637V311.364H1440V69.3637C1423.33 87.3637 1203 85.7663 911 38.3637Z" fill="#51BCDA"/>
+      </svg>
+      <Flex direction="column" justifyContent="space-between">
+        <Image
+          src="/media/petcode-logo-with-qr-code.png"
+          alt="Petcode logo with QR code"
+          width="10.25rem"
+        />
+        <Text fontSize="sm">
+          © 2020 PetCode
+        </Text>
+      </Flex>
+      <Stack spacing={2}>
+        <Text fontSize="sm">Home</Text>
+        <Text fontSize="sm">How It Works</Text>
+        <Text fontSize="sm">Contact Us</Text>
+      </Stack>
+      <Stack spacing={2}>
+        <Text fontSize="sm">FAQs</Text>
+        <Text fontSize="sm">Terms and Conditions</Text>
+        <Text fontSize="sm">Privacy Policy</Text>
+      </Stack>
+      <Stack isInline spacing={8} alignSelf="center">
+        <IconButton
+          {...SocialMediaButtonStyle}
+          aria-label="Linkedin"
+          // @ts-ignore
+          icon="linkedin"
+        />
+        <IconButton
+          {...SocialMediaButtonStyle}
+          aria-label="Instagram"
+          // @ts-ignore
+          icon="instagram"
+        />
+        <IconButton
+          {...SocialMediaButtonStyle}
+          aria-label="Facebook"
+          // @ts-ignore
+          icon="facebook"
+        />
+        <IconButton
+          {...SocialMediaButtonStyle}
+          aria-label="Youtube"
+          // @ts-ignore
+          icon="youtube"
+        />
+        <IconButton
+          {...SocialMediaButtonStyle}
+          aria-label="Pinterest"
+          // @ts-ignore
+          icon="pinterest"
+        />
+      </Stack>
     </Flex>
-  </Flex>
-);
+  );
+};
 
 export default Footer;
